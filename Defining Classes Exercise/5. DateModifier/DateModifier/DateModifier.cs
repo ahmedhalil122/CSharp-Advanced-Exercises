c538cb2ac04530de4f287d4d9c378c7cd1312427
Ahmed Halil <ahmedhalil122@gmail.com>
2023-06-02 12:01:10 +0300

    rename folder name

---
+++ Defining Classes Exercise/5. DateModifier/DateModifier/DateModifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Date_Modifier;
+
+public static class DateModifier
+{
+    public static int GetDifference(string start, string end)
+    {
+        DateTime startDate = DateTime.Parse(start);
+        DateTime endDate = DateTime.Parse(end);
+
+        TimeSpan difference = startDate.Subtract(endDate);
+
+        return Math.Abs(difference.Days);
+
+
+    }
+}
