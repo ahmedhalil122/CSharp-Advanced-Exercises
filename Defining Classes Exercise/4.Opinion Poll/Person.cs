a9d72d29dc7160e02b4458fd05fd68d41c9a8590
Ahmed Halil <ahmedhalil122@gmail.com>
2023-06-02 12:01:55 +0300

    Update Person.cs

--- Defining Classes Exercise/4.Opinion Poll/Person.cs
+++ Defining Classes Exercise/4.Opinion Poll/Person.cs
@@ -7,15 +7,7 @@ public class Person
         private string name;
         private int age;
 
-        //public Person()
-        //{
-        //    this.Name = "No name";
-        //    this.Age = 1;
-        //}
-        //public Person(int age) : this()
-        //{
-        //    this.Age = age;
-        //}
+        
         public Person(string name, int age) 
         {
             this.name = name;
