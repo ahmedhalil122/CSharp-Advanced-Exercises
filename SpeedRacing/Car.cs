0256fdce4e5f8ba0c0f1c8ef01eeb65ac1f05f0c
Ahmed Halil <ahmedhalil122@gmail.com>
2023-06-02 21:02:52 +0300

    committed new exercise

---
+++ SpeedRacing/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+	internal class Car
+	{
+		private string model;
+		private double fuelAmount;
+		private double fuelConsumptionPerKilometer;
+		private double travelledDistance;
+
+
+        public Car(string model,double fuelAmount,double fuelConsumptionPerKm)
+        {
+            this.model = model;
+			this.fuelAmount = fuelAmount;
+			this.fuelConsumptionPerKilometer = fuelConsumptionPerKm;
+        }
+        public string Model
+		{
+			get { return this.model; }
+			set { model = value; }
+		}
+
+
+        public double FuelAmount
+
+		{
+			get { return this.fuelAmount; }
+			set { fuelAmount = value; }
+		}
+
+		public double FuelConsumptionPerKilometer
+		{
+			get { return this.fuelConsumptionPerKilometer; }
+			set { fuelConsumptionPerKilometer = value; }
+
+		}
+
+		public double TravelledDistance
+		{
+			get { return this.travelledDistance; }
+			set { travelledDistance = value; }
+		}
+
+		public void DriveCar(double travelledDistance)
+		{
+			if (travelledDistance*FuelConsumptionPerKilometer>FuelAmount)
+			{
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+			else
+			{
+                FuelAmount -= travelledDistance * FuelConsumptionPerKilometer;
+                TravelledDistance += travelledDistance;
+            }
+		}
+	}
+}
---
+++ SpeedRacing/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using SpeedRacing;
+using System;
+using System.Collections.Generic;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Car> allCars = new Dictionary <string, Car>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                //AudiA4 23 0.3
+                //"{model} {fuelAmount} {fuelConsumptionFor1km}
+
+                string[] carProps = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                Car car = new(carProps[0], double.Parse(carProps[1]), double.Parse(carProps[2]));
+
+                allCars.Add(car.Model, car);
+            }
+
+            //"Drive {carModel} {amountOfKm}
+            string[] cmd = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (cmd[0]!="End")
+            {
+                string carModel = cmd[1];
+                double travelledDistance = double.Parse(cmd[2]);
+
+                Car car = allCars[carModel];
+
+                cmd = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                car.DriveCar(travelledDistance);
+            }
+
+            foreach (var car in allCars.Values)
+            {
+                //{model} {fuelAmount} {distanceTraveled
+                Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TravelledDistance}");
+            }
+
+        }
+    }
+}
