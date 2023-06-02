252e9418107835a1652ab66572779d24facbc81c
Ahmed Halil <ahmedhalil122@gmail.com>
2023-06-02 12:00:20 +0300

    adding

---
+++ Defining Classes Exercise/DateModifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Date_Modifier;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string startDate = Console.ReadLine();
+        string endDate = Console.ReadLine();
+
+        int difference = DateModifier.GetDifference(startDate, endDate);
+
+        Console.WriteLine(difference);
+
+    }
+}
