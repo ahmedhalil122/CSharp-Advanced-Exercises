05d7b5dbfcfaaab8c6fa64eead24d22de0866d27
Ahmed Halil <ahmedhalil122@gmail.com>
2023-06-09 18:10:59 +0300

    solution added

---
+++ Generics Exercises/GenericBoxOfString/Box.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString;
+
+public class Box<T>
+{
+    List<T> item = new List<T>();
+
+    public void Add(T element)
+    {
+        item.Add(element);
+    }
+
+    public override string ToString()
+    {
+       StringBuilder sb = new StringBuilder();
+        foreach (T element in item)
+        {
+            sb.AppendLine($"{typeof(string)}: {element}");
+        }
+            return sb.ToString();
+    }
+}
---
+++ Generics Exercises/GenericBoxOfString/Program.cs
@@ -0,0 +1,19 @@
+﻿using GenericBoxOfString;
+
+namespace StartUp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+        Box<string> box = new();
+        for (int i = 0; i < count; i++)
+        {
+            string input = Console.ReadLine();
+            box.Add(input);
+        }
+
+        Console.WriteLine(box.ToString());
+    }
+}
