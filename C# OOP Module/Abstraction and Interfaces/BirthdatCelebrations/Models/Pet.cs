be0c846f3ae8fae74b0e1039df7dc8d3c9f8b6af
Ahmed Halil <ahmedhalil122@gmail.com>
2023-07-06 03:29:25 +0300

    added solution

---
+++ C# OOP Module/Abstraction and Interfaces/BirthdatCelebrations/Models/Pet.cs
@@ -0,0 +1,15 @@
+﻿using BorderControl.Models.Interfaces;
+
+namespace BorderControl.Models
+{
+    public class Pet : INamable, IBirthable
+    {
+        public Pet(string name, string birhtdate)
+        {
+            Name = name;
+            Birthdate = birhtdate;
+        }
+        public string Name { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
